57352c5651640c8715bfb5855e0bdfea1ec8c9b8
Jeremy Zevin <jz@Jeremys-MBP.fios-router.home>
2018-11-18 16:41:57 -0500

    added proper return types on methods

--- src/TinyVector.ts
+++ src/TinyVector.ts
@@ -17,37 +17,37 @@ export class TinyVector {
     this.x = x;
     this.y = y;
   }
-  add(tv:TinyVector){
+  add(tv:TinyVector): TinyVector {
     this.x += tv.x;
     this.y += tv.y;
     return this;
   }
-  sub(tv:TinyVector){
+  sub(tv:TinyVector): TinyVector {
     this.x -= tv.x;
     this.y -= tv.y;
     return this;
   }
-  mult(n){
+  mult(n): TinyVector {
     this.x *= n;
     this.y *= n;
     return this;
   }
-  div(n){
+  div(n): TinyVector {
     this.x /= n;
     this.y /= n;
     return this;
   }
-  mag(){
+  mag(): number{
     return Math.sqrt((this.x * this.x) + (this.y * this.y));
   }
-  normalize(){
+  normalize(): TinyVector {
     const m = this.mag();
     if (m !== 0) {
       this.div(m);
     }
     return this;
   }
-  limit(h, l) {
+  limit(h, l): TinyVector {
     const high = h || null,
         low = l || null;
     if (high && this.mag() > high) {
@@ -60,29 +60,29 @@ export class TinyVector {
     }
     return this;
   }
-  zero(){
+  zero(): TinyVector {
     this.x = 0;
     this.y = 0;
     return this;
   }
-  clone(){
+  clone(): TinyVector {
     return new TinyVector(this.x,this.y);
   }
-  rotate(angle:number){
+  rotate(angle:number): TinyVector {
     const nx = this.x * Math.cos(angle) - this.y * Math.sin(angle);
     const ny = this.x * Math.sin(angle) + this.y * Math.cos(angle);
     this.x = nx;
     this.y = ny;
     return this;
   }
-  rotateDeg(angle:number){
+  rotateDeg(angle:number): TinyVector {
     return this.rotate(this._degrees2radian(angle));
   }
 
-  private _radian2degrees (rad:number) {
+  private _radian2degrees (rad:number): number{
       return rad * (180 / Math.PI);
   }
-  private _degrees2radian(deg:number) {
+  private _degrees2radian(deg:number): number {
       return deg / (180 / Math.PI);
   }
 }
